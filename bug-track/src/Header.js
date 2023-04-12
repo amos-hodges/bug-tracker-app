0e831e61c6ea203734906bafa432317f44d6e5ed
Amos Hodges <amos.v.hodges@gmail.com>
2023-04-12 14:36:12 -0400

    adding root directory

--- bug-track/src/Header.js
+++ bug-track/src/Header.js
@@ -1,14 +1,18 @@
 
-const Header = () => {
+const Header = ({ title }) => {
     return (
         // Styles can be written inline, probably easiest to keep it in CSS
         <header style={{
             backgroundColor: 'Mediumblue',
             color: '#fff'
         }}>
-            <h1>Bug Tracker To-Do</h1>
+            <h1>{title}</h1>
         </header>
     )
+}
+
+Header.defaultProps = {
+    title: "Default Title"
 }
 
 export default Header
