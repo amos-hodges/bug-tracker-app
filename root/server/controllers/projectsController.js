03fef3e0e191a9536d949ae4169a1d27a86d048f
Amos Hodges <111532533+amos-hodges@users.noreply.github.com>
2023-05-12 07:24:23 -0400

    added logic for getAllProjects and createNewProject

--- root/server/controllers/projectsController.js
+++ root/server/controllers/projectsController.js
@@ -5,12 +5,55 @@ const User = require('../models/User')
 // @desc Get all projects
 // @route GET /projects
 // @access Private
-const getAllProjects = async (req, res) => { }
+
+const getAllProjects = async (req, res) => {
+    const projects = await Project.find().lean()
+    if (!projects?.length) {
+        return res.status(400).json({ message: 'No projects found' })
+    }
+
+    const projectsWithUsersAndTickets = await Promise.all(
+        projects.map(async (project) => {
+            const populatedProject = await Project.populate(project, [
+                { path: 'tickets', model: Ticket },
+                { path: 'users', model: User }
+            ])
+            return populatedProject
+        })
+    )
+    res.json(populatedProject)
+}
 
 // @desc Create a new project
 // @route POST /projects
 // @access Private
-const createNewProject = async (req, res) => { }
+const createNewProject = async (req, res) => {
+    const { title, users } = req.body
+
+    if (!title) {
+        return res.status(400).json({ message: 'Title field is required' })
+    }
+
+    //check duplicates
+    const duplicate = await Ticket.findOne({ title }).collation({ locale: 'en', strength: 2 }).lean().exec()
+
+    if (duplicate) {
+        return res.status(409).json({ message: 'Duplicate project title' })
+    }
+
+    const project = await Project.create({
+        title,
+        users: users || [],
+        tickets: [],
+    });
+
+    if (project) {
+        return res.status(201).json({ message: 'Project succesfuly created' })
+    } else {
+        return res.status(400).json({ message: 'Invalid project data recieved' })
+    }
+
+}
 
 // @desc Update a project
 // @route PATCH /projects
