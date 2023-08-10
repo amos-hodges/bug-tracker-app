26d2c7f46c4226981fac4a409aef2593def00023
Amos Hodges <amos.v.hodges@gmail.com>
2023-08-10 16:58:29 -0400

    fixed glitch with loading bar, working on styling grid

--- root/client/src/components/Grid.js
+++ root/client/src/components/Grid.js
@@ -8,7 +8,16 @@ const Grid = ({ data, keyFn, navFn }) => {
         setExpanded(cardId === expanded ? null : cardId)
     }
     const currentProject = data.filter(row => row.id === expanded)[0]
-    const percentageComplete = (currentProject?.completeTicketCount / currentProject?.ticketCount * 100).toFixed(2)
+
+    let percentageComplete
+    if (currentProject?.completeTicketCount > 0
+        && currentProject?.ticketCount > 0) {
+        percentageComplete = (currentProject?.completeTicketCount /
+            currentProject?.ticketCount * 100).toFixed(2)
+    } else {
+        percentageComplete = 0
+    }
+
 
     const renderedCards = data.map((rowData) => {
 
@@ -29,18 +38,21 @@ const Grid = ({ data, keyFn, navFn }) => {
 
     return (
         <div>
-
             <div className="card-grid">
                 {renderedCards}
             </div>
             {expanded &&
                 <div className="card-content">
                     <h2>{currentProject.title}</h2>
-                    <h4>{currentProject.description}</h4>
+                    <div className="card-section">
+                        <h4>{currentProject.description}</h4>
+                        <div className="button-18" onClick={() => navFn(currentProject.id)}>View Users</div>
+                    </div>
+
                     <div className="card-options">
-                        <div>List of users assinged...</div>
                         <ProgressBar percentage={percentageComplete} />
                         <div>{`${currentProject?.completeTicketCount || 0}/${currentProject?.ticketCount || 0} tickets completed.`}</div>
+
                         <div className="button-18" onClick={() => navFn(currentProject.id)}>Open Project</div>
                     </div>
                 </div>}
